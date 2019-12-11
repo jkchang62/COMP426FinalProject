2ed18fb8392bccec8cbed0d693f5608443af2949
Jonathan Chang <jkchang0@live.unc.edu>
2019-12-11 16:39:46 -0500

    More changes to the primary header and fixed signout design

--- src/Components/PrimaryHeader.js
+++ src/Components/PrimaryHeader.js
@@ -1,67 +1,16 @@
 import React from 'react';
 import './PrimaryHeader.css';
-import Navigation from './Navigation';
 import { AuthUserContext } from './sessions';
 import AppBar from '@material-ui/core/AppBar';
-import { fade } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Link } from 'react-router-dom';
 import SignOut from './signOut';
-import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 7),
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: 120,
-            '&:focus': {
-                width: 200,
-            },
-        },
-    },
+    }
 }));
 
 const SearchAppBar = () => {
@@ -74,24 +23,12 @@ const SearchAppBar = () => {
                     <div className = "logo-container">
                       <img src = 'https://upload.wikimedia.org/wikipedia/en/5/5f/Project_M_logo.png' />
                     </div>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
+                    
+                    <div className="navigation-container">
+                        <div>
+                            <Link to="/DashBoard">Dashboard | </Link>
+                            <SignOut />
                         </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{ 'aria-label': 'search' }}
-                        />
-                    </div>
-                    <div className = "navigation-container">
-                    <div>
-                        <Link to="/DashBoard">Dashboard</Link>
-                        <SignOut />
-                    </div>
                     </div>
                 </Toolbar>
             </AppBar>
@@ -116,13 +53,11 @@ const DefaultHeader = () => {
 
 export default function PrimaryHeader() {
     const classes = useStyles();
-
     return (
         <div className={classes.root}>
             <AuthUserContext.Consumer>
                 {authUser => authUser ? <SearchAppBar /> : <DefaultHeader />}
             </AuthUserContext.Consumer>
-
         </div>
     );
 
--- src/Components/signOut.js
+++ src/Components/signOut.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { withFirebase } from '../Components/index';
-import { Link } from 'react-router-dom';
 import './SignOut.css';
+import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
+import { withFirebase } from '../Components/index';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const SignOutEvent = ({ firebase }) => (
     <Link to='/MainPage'>
         <Button id = "sign-out-button" type="button" onClick={firebase.SignOutUser}>
             Sign-Out
+            <ExitToAppIcon />
         </Button>
     </Link>
 );
